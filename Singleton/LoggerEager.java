785a415b51a5ea54834d9a708438beef69856210
Cameron Howard <camhoward93@gmail.com>
2020-09-04 19:16:00 -0700

    Update LoggerEager.java

--- Singleton/LoggerEager.java
+++ Singleton/LoggerEager.java
@@ -1,11 +1,18 @@
 package singleton;
 
+/*
+The eager logger singleton will rely on the JVM to create the unique instance when the class is loaded.
+ */
 public class LoggerEager {
     private static LoggerEager uniqueInstance = new LoggerEager();
 
     private LoggerEager(){}
 
-    public static synchronized LoggerEager getInstance(){
+    public static LoggerEager getInstance(){
         return uniqueInstance;
+    }
+
+    public static void log(String s){
+        System.out.println(s);
     }
 }
